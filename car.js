c81b38cce29b866efc3a6ebd61e9aaa89363cf3e
Swati <swati23793@gmail.com>
2020-01-19 21:23:47 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,29 @@
+function checkDriverAge(){
+  alert("You want to Drive a Car? But Are you eligible? Let's Check it Out!");
+  var age = 18;
+  var eligible = prompt("How old Are You?");
+  if (eligible > age)
+  {
+    alert("Powering On. Enjoy the ride!");
+  } else if (eligible < age){
+    alert("Sorry, you are too young to drive this car. Powering off");
+  }
+  else{
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+//checkDriverAge();
+
+function checkDriverAge2(age){
+  var age;
+  if (age > 18)
+  {
+    alert("Powering On. Enjoy the ride!");
+  } else if (age < 18){
+    alert("Sorry, you are too young to drive this car. Powering off");
+  }
+  else{
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+checkDriverAge2();
