94d0ecfb3683666422d8024e06007563ef406c88
Swati <swati23793@gmail.com>
2020-01-19 21:26:56 +0530

    Update car.js

--- car.js
+++ car.js
@@ -12,18 +12,18 @@ function checkDriverAge(){
     alert("Congratulations on your first year of driving. Enjoy the ride!");
   }
 }
-//checkDriverAge();
+checkDriverAge();
 
 function checkDriverAge2(age){
   var age;
   if (age > 18)
   {
-    alert("Powering On. Enjoy the ride!");
+    return ("Powering On. Enjoy the ride!");
   } else if (age < 18){
-    alert("Sorry, you are too young to drive this car. Powering off");
+    return ("Sorry, you are too young to drive this car. Powering off");
   }
   else{
-    alert("Congratulations on your first year of driving. Enjoy the ride!");
+    return ("Congratulations on your first year of driving. Enjoy the ride!");
   }
 }
 checkDriverAge2();
